938b10d3228a2d7484c4e24192f7651cd8e0f68c
Jingkun Liu <l.jingkun@gmail.com>
2020-05-21 20:55:50 +0800

    test

--- assets/cases/demo/ray-cast-reflection.js
+++ assets/cases/demo/ray-cast-reflection.js
@@ -15,19 +15,88 @@ cc.Class({
 
     // called every frame, uncomment this function to activate update callback
     update: function (dt) {
-
         this.angle += Math.PI / 20 * dt;
-
         var p1 = this.center;
-        var p2 = cc.v2(Math.cos(this.angle), Math.sin(this.angle)).mulSelf(this.radius).addSelf(this.center);
 
+
+        /*
+        var p2 = cc.v2(Math.cos(this.angle), Math.sin(this.angle)).mulSelf(this.radius).addSelf(this.center);
         this.ctx.clear();
-    
         this.remainLength = this.radius;
         this.rayCast(p1, p2);
+        */
+
+       //this.rayCast0(p1)
+       this.testRayCast0(p1);
+    },
+
+
+    testRayCast0: function(p1) {
+        this.ctx.clear();
+        this.remainLength = this.radius;
+
+        var dirVec = cc.v2(Math.cos(this.angle), Math.sin(this.angle));
+        this.testRayCast(p1, dirVec)
+    },
+
+    testRayCast: function(startLocation, dirVec) {
+        if (this.remainLength <= 0) {
+            return;
+        }
+
+        var endLocation = startLocation.add(dirVec.mul(this.remainLength));
+
+        var manager = cc.director.getPhysicsManager();
+        var results = manager.rayCast(startLocation, endLocation);
+
+        if (results.length > 0) {
+            var result = results[0];
+            var tPoint = result.point;
+            this.drawAimLine(startLocation, tPoint);
+
+            var tDrawLen = tPoint.sub(startLocation).mag();
+            this.remainLength -= tDrawLen;
+
+            //const vector_r = vector_i.sub(vector_n.mul(2 * vector_i.dot(vector_n)));
+            var normalVec = result.normal;
+            var reflectDirVec = dirVec.sub(normalVec.mul(2 * dirVec.dot(normalVec)));
+            this.testRayCast(tPoint, reflectDirVec);
+        } else {
+            this.drawAimLine(startLocation, endLocation);
+        }
+    },
+
+
+    drawAimLine: function(startLocation, endLocation) {
+
+        startLocation = cc.v2(startLocation.x, startLocation.y);
+
+        this.ctx.moveTo(startLocation.x, startLocation.y);
+
+        var delta    = 20;
+        var dirVec   = endLocation.sub(startLocation);
+        var totalCnt = Math.round(dirVec.mag() / delta);
+        //console.log("######cnt:", totalCnt);
+
+        dirVec.normalizeSelf().mulSelf(delta);
+
+        for (var i = 0; i < totalCnt; i++) {
+            startLocation.addSelf(dirVec);
+            this.ctx.circle(startLocation.x, startLocation.y, 2);
+            this.ctx.fill();
+        }
 
     },
 
+
+
+    rayCast0: function(p1) {
+        this.ctx.clear();
+        this.remainLength = this.radius;
+
+        var p2 = cc.v2(Math.cos(this.angle), Math.sin(this.angle)).mulSelf(this.radius).addSelf(this.center);        
+        this.rayCast(p1, p2);
+    },
     rayCast: function (p1, p2) {
         var manager = cc.director.getPhysicsManager();
         var result = manager.rayCast(p1, p2)[0];
